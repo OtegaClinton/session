71bddad9855c00f54464ad4367a6fc2f0144065b
OtegaClinton <oghenetegaokotie@gmail.com>
2024-08-02 08:45:42 +0100

    creating user with data from google

--- router/router.js
+++ router/router.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const {createUser,homePage,logIn,logout,signGoogle,redirect} = require("../controllers/userController");
 const router = require("express").Router();
+const userModel = require("../models/userModel");
 
 
 
@@ -14,20 +15,42 @@ router.get("/googlesignup",signGoogle);
 router.get("/google/callback",redirect);
 
 
-router.get("/auth/google/success",async(req,res) =>{
+router.get("/auth/google/success", async (req, res) => {
     try {
-     console.log(req.user._json);
+        if (req.user && req.user._json) {
+            const { email, name, picture } = req.user._json;
 
-       res.send(`successful`);
-        
+            // Check if the user already exists
+            const existingUser = await userModel.findOne({ email });
+            if (existingUser) {
+                return res.status(200).json({ 
+                    message: `User with email ${email} already exists`
+                });
+            }
+
+            // Create a new user
+            const user = await userModel.create({
+                fullName: name,
+                picture: picture,
+                email: email
+            });
+
+            return res.status(201).json({ 
+                message: 'Successfully created', 
+                data: user 
+            });
+
+        } else {
+            return res.status(400).json({ 
+                message: 'User data not found' 
+            });
+        }
     } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
-                 
+        return res.status(500).json({ 
+            message: error.message 
+        });
     }
-
-})
+});
 
 
 module.exports = router;
